a92fd5affe7ff16d6c7a1198d48e47968868c063
Bruno Galeotti <bruno.galeotti@aerolab.co>
2020-04-18 22:04:20 -0300

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,45 @@
+// @ts-nocheck
+import { addPlugin as addFlipperPlugin } from 'react-native-flipper';
+import { CachedQuery, QueryCache } from 'react-query';
+
+export function addPlugin(queryCache: QueryCache) {
+  let unsubscribe: (() => void) | undefined;
+
+  function getQueries(): CachedQuery[] {
+    return queryCache.getQueries(() => true);
+  }
+
+  function getQueryByHash(queryHash: string): CachedQuery | undefined {
+    return queryCache.getQueries(
+      (query: CachedQuery) => query.queryHash === queryHash
+    )[0];
+  }
+
+  addFlipperPlugin({
+    getId: () => 'flipper-plugin-react-query-devtools',
+    onConnect(connection) {
+      unsubscribe = queryCache.subscribe(() => {
+        connection.send('queries', getQueries());
+      });
+
+      connection.receive('query:refetch', (queryHash, responder) => {
+        getQueryByHash(queryHash)?.fetch();
+        responder.success();
+      });
+      connection.receive('query:remove', (queryHash, responder) => {
+        queryCache.removeQueries(query => query.queryHash === queryHash);
+        responder.success();
+      });
+
+      // send initial queries
+      connection.send('queries', getQueries());
+    },
+    onDisconnect() {
+      if (unsubscribe) {
+        unsubscribe();
+        unsubscribe = undefined;
+      }
+    },
+    runInBackground: () => true,
+  });
+}
