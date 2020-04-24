128186f30c251f7274a87d4a6098d16777a50b11
Bruno Galeotti <bruno.galeotti@aerolab.co>
2020-04-24 09:43:06 -0300

    fix: format zero timestamp

--- packages/flipper-plugin-react-query-native-devtools/src/utils/index.ts
+++ packages/flipper-plugin-react-query-native-devtools/src/utils/index.ts
@@ -3,6 +3,10 @@ import padStart from 'lodash/padStart';
 import { Query } from '../types';
 
 export function formatTimestamp(timestamp: number): string {
+  if (timestamp === 0) {
+    return '-';
+  }
+
   const date = new Date(timestamp);
 
   return `${padStart(date.getHours().toString(), 2, '0')}:${padStart(date.getMinutes().toString(), 2, '0')}:${padStart(
