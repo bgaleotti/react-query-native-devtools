9200c5d9ccb8b3b8e34ac3003167d5b778e51bd2
Bruno Galeotti <bgaleotti@gmail.com>
2020-05-27 10:25:34 -0300

    fix: remove `cache` property from queries before sending to client
    Fixes #9.

--- packages/react-query-native-devtools/types/index.d.ts
+++ packages/react-query-native-devtools/types/index.d.ts
@@ -4,6 +4,7 @@ declare module "react-query" {
   export interface CachedQuery extends ReactQuery.CachedQuery {
     queryHash: string,
     fetch: <TResult>({ force }?: { force?: boolean }) => Promise<TResult>,
+    cache: unknown,
   }
 
   export interface QueryCache extends ReactQuery.QueryCache {
