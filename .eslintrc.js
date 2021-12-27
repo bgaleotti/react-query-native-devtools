2543ca6610907d15207b045d16f7034e9ed62c14
Denis Irkhin <denis.irkhin@logmein.com>
2021-12-26 21:32:21 -0500

    feat(plugin): move lint and commitlint to root

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['simple-import-sort', '@typescript-eslint', 'react-hooks'],
+  rules: {
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-empty-function': ['error', { allow: ['arrowFunctions'] }],
+    '@typescript-eslint/no-use-before-define': 'warn',
+    curly: 'warn',
+    camelcase: 'off',
+    'linebreak-style': ['error', 'unix'],
+    'newline-before-return': 'error',
+    // FIXME: revert
+    'no-console': 'warn',
+    'react/prop-types': 'off',
+    'simple-import-sort/imports': 'error',
+    'sort-imports': 'off',
+    strict: ['error', 'global'],
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
+  settings: {
+    'import/parsers': {
+      '@typescript-eslint/parser': ['.ts', '.tsx'],
+    },
+    'import/resolver': {
+      node: {
+        extensions: ['.ts', '.tsx'],
+      },
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+};
